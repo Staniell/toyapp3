4d2cdb66a5523734929fce257b0aa10cb538c97c
staniell <giostaniell12@gmail.com>
2023-03-18 17:19:26 +0800

    admin features done

--- app/javascript/posts.js
+++ app/javascript/posts.js
@@ -1,7 +1,7 @@
 document.addEventListener("turbo:load", function () {
   // Get the file input field
   const input = document.querySelector('input[type="file"]');
-  
+
   document.getElementById("image-preview").innerHTML = "";
 
   // Listen for changes to the file input field
@@ -19,13 +19,16 @@ document.addEventListener("turbo:load", function () {
       // Listen for the FileReader to load
       reader.addEventListener("load", () => {
         // Create a new image element
+        const swiperDiv = document.createElement("div");
         const img = document.createElement("img");
-        img.classList.add("object-cover");
+        img.classList.add("object-cover", "w-64 h-64 m-auto");
+        swiperDiv.classList.add("swiper-slide");
         // Set the image source to the loaded file data
         img.src = reader.result;
 
+        swiperDiv.appendChild(img);
         // Append the image to the preview container
-        document.getElementById("image-preview").appendChild(img);
+        document.getElementById("image-preview").appendChild(swiperDiv);
       });
 
       // Read the selected file as a data URL
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,24 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # root "pages#home"
+
+  authenticated :user, ->(user) { user.admin? } do
+    get "admin", to: "admin#index"
+    get "admin/new_tag"
+    post "admin/new_tag", to: "admin#create_tag"
+    get "admin/edit_tag/:id", to: "admin#edit_tag"
+    post "admin/edit_tag/:id", to: "admin#update_tag"
+    get "admin/comment_check" 
+    get "admin/post_check"
+    get "admin/user_check"
+    get "admin/post_by_user/:id", to: "admin#post_by_user"
+    delete "admin/post_by_user/:id", to: "admin#destroy_post"
+    delete "admin/user_check/:id", to: "admin#destroy_user"
+    delete "admin/post_check/:id", to: "admin#destroy_post"
+    delete "admin/comment_check/:id", to: "admin#destroy_comment"
+    delete "admin/destroy_tag/:id", to: "admin#destroy_tag"
+  end
+
   devise_for :users
   resources :users, only: [:show, :destroy] do
     resources :posts, only: [:index]
