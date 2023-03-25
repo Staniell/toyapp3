f582d30d72c04651883db5bfb14a0ebe3cac980f
staniell <giostaniell12@gmail.com>
2023-03-26 01:40:10 +0800

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "pages#home"
   devise_for :users
+  "testing"
   # Defines the root path route ("/")
   # root "articles#index"
 end
